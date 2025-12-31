5abb0489e0a36f0113b41770081fbab142fc5646
Masao Takaku <tmasao@acm.org>
2026-01-01 00:45:34 +0900

    add filter script

---
+++ shacl_result_filter.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require 'rdf/turtle'
+require 'pp'
+
+graph = RDF::Graph.new do |g|
+  RDF::Turtle::Reader.new(ARGF.read){|reader|
+    g << reader
+  }
+end
+p graph.count
+query = RDF::Query.new({
+  result: {
+    RDF.type => RDF::URI("http://www.w3.org/ns/shacl#ValidationResult"),
+    RDF::URI("http://www.w3.org/ns/shacl#focusNode") => :focus,
+    RDF::URI("http://www.w3.org/ns/shacl#resultMessage") => :message,
+    RDF::URI("http://www.w3.org/ns/shacl#resultPath") => :path,
+    RDF::URI("http://www.w3.org/ns/shacl#resultSeverity") => :severity,
+    RDF::URI("http://www.w3.org/ns/shacl#sourceConstraintComponent") => :constraint,
+    RDF::URI("http://www.w3.org/ns/shacl#sourceShape") => :shape, 
+    RDF::URI("http://www.w3.org/ns/shacl#value") => :value,
+  }
+})
+query.execute(graph).each do |result|
+  next if result.message.to_s =~ /\AValue does not have class cs:/
+  pp result.to_h
+end
